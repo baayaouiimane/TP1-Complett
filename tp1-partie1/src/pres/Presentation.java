bb3553a1d298ca567ebdc4f7484bc98bcefd3fd9
baayaouiimane <167249908+baayaouiimane@users.noreply.github.com>
2024-05-16 02:07:10 +0100

    Add files via upload

---
+++ tp1-partie1/src/pres/Presentation.java
@@ -0,0 +1,22 @@
+package pres;
+
+import dao.DaoImpl;
+import ext.DaoImpl2;
+import ext.DaoImplVWS;
+import metier.MetierImpl;
+
+public class Presentation {
+
+    public static void main(String[] args){
+
+        /*
+        injection des dependances par instanciation
+        dynamique => new
+         */
+        //DaoImpl2 dao = new DaoImpl2();
+        DaoImplVWS dao = new DaoImplVWS();
+        MetierImpl metier = new MetierImpl();
+        metier.setDao(dao);
+        System.out.println("resultat = " + metier.calcul());
+    }
+}
